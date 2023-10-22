37400206aa119d901671071c0a5c80fc6a035cc1
Banh Mi <longkim223300@gmail.com>
2023-10-22 23:49:06 +0700

    Fix file pageRender.jsx line 23

--- client/src/config/routes/PageRender.jsx
+++ client/src/config/routes/PageRender.jsx
@@ -1,29 +1,28 @@
-import { useParams, useLocation } from 'react-router-dom';
-import NotFound from '../../pages/NotFound';
-import { useEffect, useState } from 'react';
+import { useParams, useLocation } from 'react-router-dom'
+import NotFound from '../../pages/NotFound'
+import { useEffect, useState } from 'react'
 
 function PageRender() {
-    const { page, id } = useParams();
-    const location = useLocation();
-    // const pathName = location.pathname;
-    const [PageComponent, setPageComponent] = useState(null);
-    const pageName = id
-        ? `${page?.replace(/\w/, page?.charAt(0).toUpperCase())}/[id]`
-        : page?.replace(/\w/, page?.charAt(0).toUpperCase()); 
-       
-        useEffect(() => {
-            import(/* @vite-ignore */`../../pages/${pageName}`)
-            .then(module => {
-                const PageComponent = module.default;
-                setPageComponent(PageComponent);
-            })
-            .catch((e) => {
-                setPageComponent(NotFound);    
-            });
-        }, [])
-     
-        if(PageComponent)
-        return PageComponent;
+	const { page, id } = useParams()
+	const location = useLocation()
+	// const pathName = location.pathname;
+	const [PageComponent, setPageComponent] = useState(null)
+	const pageName = id
+		? `${page?.replace(/\w/, page?.charAt(0).toUpperCase())}/[id]`
+		: page?.replace(/\w/, page?.charAt(0).toUpperCase())
+
+	useEffect(() => {
+		import(/* @vite-ignore */ `../../pages/${pageName}`)
+			.then((module) => {
+				const PageComponent = module.default
+				setPageComponent(PageComponent)
+			})
+			.catch((e) => {
+				setPageComponent(NotFound)
+			})
+	}, [page])
+
+	if (PageComponent) return PageComponent
 }
 
-export default PageRender;
+export default PageRender
